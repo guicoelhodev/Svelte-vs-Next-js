1e75180040e524934425e74c672f502f7780b294
guicoelhodev <gs.coelho_dev@outlook.com>
2024-12-05 12:24:41 +0000

    feat(Svelte): add async handler on homepage to load content even without javascript

--- svelte/src/routes/+page.server.ts
+++ svelte/src/routes/+page.server.ts
@@ -1,7 +1,7 @@
 import { api } from "$lib/services/api";
 
 export async function load() {
-  const response = api.getCharacters(1);
+  const response = await api.getCharacters(1);
   return {
     response,
   };
